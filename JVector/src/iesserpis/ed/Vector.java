fd5c1b87b8ebdf9e4023c021c6f1fcbe725712c9
yoelgarridodam <garridoalmelayoel@gmail.com>
2019-10-28 13:44:55 +0100

    JVector mejorado con bucles

--- JVector/src/iesserpis/ed/Vector.java
+++ JVector/src/iesserpis/ed/Vector.java
@@ -4,11 +4,34 @@ public class Vector {
 
 	public static void main(String[] args) {
 		
-		int[] v = {17, 12, 15, 9, 14};
+//		int[] v = {17, 12, 15, 9, 14};
 		
-		for (int index = 0; index < 5; index++)
-		System.out.println("elemento " + index + " valor " + v[index]);
+//		int index = 0;
+//		while (index < 5) {
+//			System.out.println(index);
+//		}
+		
+		int[] v = new int[] {17, 12, 15, 9, 14};
+		int x = 33;
+		int index = indexOf(v, x);
+		System.out.println("index = " + index);
+		System.out.println("fin");
+		
+			
+	}
 
+	private static int indexOf(int[] v, int x) {
+/*		int index = 0;
+		while (index < v.length && v[index] != x)
+			index++;
+		if (index == v.length)
+			return -1;
+		return index;
+*/
+		for (int index = 0; index < v.length; index++)
+			if (v[index] == x)
+				return index;
+		return -1; 
 	}
 
 }
